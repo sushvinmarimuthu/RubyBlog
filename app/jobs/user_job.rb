fbc1ba3adf74f4785d627dc9f22628ec08847cb1
Sushvin MariMuthu <sushvinmarimuthu@gmail.com>
2023-05-01 10:18:03 +0530

    Updated

--- app/jobs/user_job.rb
+++ app/jobs/user_job.rb
@@ -1,7 +1,2 @@
 class UserJob < ApplicationJob
-  queue_as :default
-
-  def perform(user)
-    UserMailer.welcome_email(user).deliver_now
-  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,2 @@
 class UserMailer < ApplicationMailer
-  default from: 'mug.sushvin@gmail.com'
-
-  def welcome_email(user)
-    @user = user
-    mail(to: @user.email, subject: 'Welcome to RubyBlog')
-  end
 end
